bce02bfc193a621e1c9a312a0af8bf0f90d6a014
Jared Hanson <jaredhanson@gmail.com>
2018-05-26 09:10:44 -0700

    Add tests.

--- test/unseal.test.js
+++ test/unseal.test.js
@@ -9,56 +9,108 @@ var expect = require('chai').expect;
 describe('unseal', function() {
   
   describe('using defaults', function() {
-    var unseal, keying;
     
     describe('decrypting', function() {
-      before(function() {
-        keying = sinon.spy(function(entity, q, cb){
-          if (q.usage == 'decrypt') {
-            return cb(null, { secret: 'abcdef7890abcdef' });
-          } else {
-            return cb(null, { secret: 'API-12abcdef7890' });
-          }
-        });
+      var claims, conditions;
       
-        unseal = setup(keying);
+      var keying = sinon.spy(function(entity, q, cb){
+        if (q.usage == 'decrypt') {
+          return cb(null, { secret: 'abcdef7890abcdef' });
+        } else {
+          return cb(null, { secret: 'API-12abcdef7890' });
+        }
       });
       
-      var tkn;
       before(function(done) {
         var token = 'gAAAAABZQZ8EjPXNpNuS1P2retbZFG9yvR068ZRVdw2ba0JXJdrRxaqkuqKU5kgchw2So0T8HMBSowFnrjnyP4XFTOfHp-6ttg==';
         
-        unseal(token, function(err, t) {
-          tkn = t;
+        var unseal = setup(keying);
+        unseal(token, function(err, c, co) {
+          claims = c;
+          conditions = co;
           done(err);
         });
-      });
-      
-      after(function() {
-        keying.reset();
       });
       
       it('should query for key', function() {
         expect(keying.callCount).to.equal(2);
         var call = keying.getCall(0);
+        expect(call.args[0]).to.be.undefined;
         expect(call.args[1]).to.deep.equal({
           usage: 'decrypt',
           algorithms: [ 'aes128-cbc' ],
         });
         
         call = keying.getCall(1);
+        expect(call.args[0]).to.be.undefined;
         expect(call.args[1]).to.deep.equal({
           usage: 'verify',
           algorithms: [ 'hmac-sha256' ],
         });
       });
       
-      it('should unseal token', function() {
-        expect(tkn).to.deep.equal({
-            foo: 'bar'
+      it('should yield claims', function() {
+        expect(claims).to.deep.equal({
+          foo: 'bar'
+        });
+      });
+      
+      it('should yield conditions', function() {
+        expect(conditions).to.deep.equal({
         });
       });
     }); // decrypting
+    
+    describe('decrypting with issuer', function() {
+      var claims, conditions;
+      
+      var keying = sinon.spy(function(entity, q, cb){
+        if (q.usage == 'decrypt') {
+          return cb(null, { secret: 'abcdef7890abcdef' });
+        } else {
+          return cb(null, { secret: 'API-12abcdef7890' });
+        }
+      });
+      
+      before(function(done) {
+        var token = 'gAAAAABZQZ8EjPXNpNuS1P2retbZFG9yvR068ZRVdw2ba0JXJdrRxaqkuqKU5kgchw2So0T8HMBSowFnrjnyP4XFTOfHp-6ttg==';
+        
+        var unseal = setup(keying);
+        unseal(token, { issuer: { identifier: 'https://server.example.com' } }, function(err, c, co) {
+          claims = c;
+          conditions = co;
+          done(err);
+        });
+      });
+      
+      it('should query for key', function() {
+        expect(keying.callCount).to.equal(2);
+        var call = keying.getCall(0);
+        expect(call.args[0]).to.deep.equal({ identifier: 'https://server.example.com' });
+        expect(call.args[1]).to.deep.equal({
+          usage: 'decrypt',
+          algorithms: [ 'aes128-cbc' ],
+        });
+        
+        call = keying.getCall(1);
+        expect(call.args[0]).to.deep.equal({ identifier: 'https://server.example.com' });
+        expect(call.args[1]).to.deep.equal({
+          usage: 'verify',
+          algorithms: [ 'hmac-sha256' ],
+        });
+      });
+      
+      it('should yield claims', function() {
+        expect(claims).to.deep.equal({
+          foo: 'bar'
+        });
+      });
+      
+      it('should yield conditions', function() {
+        expect(conditions).to.deep.equal({
+        });
+      });
+    }); // decrypting with issuer
     
   }); // using defaults
   
